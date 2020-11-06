2208554aa0f01dd3200d2cd977d483b3b25b253b
zotacy <tacyzacharyo@johndeere.com>
2020-11-06 00:35:02 -0500

    Fixed showWorkout bug. removed <%=user.id%> from form action in workoutForm and delete

--- seeders/20201102154141-demo-workouts.js
+++ seeders/20201102154141-demo-workouts.js
@@ -12,6 +12,22 @@ module.exports = {
           activity_notes: "Keep it EASY.",
           userId: "1",
         },
+        {
+          activity_name: "Example: Moderate Bike Workout",
+          exerciseId: "2",
+          difficultyId: "2",
+          activity_description: "Easy Bike, 1 hr.",
+          activity_notes: "Keep it Steady.",
+          userId: "1",
+        },
+        {
+          activity_name: "Example: Hard Workout",
+          exerciseId: "3",
+          difficultyId: "3",
+          activity_description: "Hard Swim 1 hr.",
+          activity_notes: "Make it Hard!",
+          userId: "1",
+        },
       ],
       {}
     );
